42d53f6825310f473637d6e0cf5ab7981b8235ca
Kooreis <kooreis.official@gmail.com>
2024-08-15 13:49:48 -0500

    This is the memoize function. It is a higher-order function that takes a function as an argument and returns a new function that caches the results of the input function. This is useful for expensive functions that are called multiple times with the same arguments.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,3 @@
-Here is a simple example of how you can implement memoization for an expensive recursive function in JavaScript. In this example, we will use the Fibonacci sequence, which is a common example of an expensive recursive function.
-
-```javascript
 function memoize(fn) {
     const cache = {};
     return function(...args) {
@@ -12,18 +9,3 @@ function memoize(fn) {
         return result;
     };
 }
-
-function expensiveRecursiveFunction(n) {
-    if (n < 2){
-        return n
-    }
-    return memoizedExpensiveRecursiveFunction(n - 1) + memoizedExpensiveRecursiveFunction(n - 2);
-}
-
-const memoizedExpensiveRecursiveFunction = memoize(expensiveRecursiveFunction);
-
-console.log(memoizedExpensiveRecursiveFunction(10));  // 55
-console.log(memoizedExpensiveRecursiveFunction(50));  // 12586269025
-```
-
-In this code, `memoize` is a higher-order function that takes an expensive function as an argument and returns a new function that caches the results of the expensive function. The `expensiveRecursiveFunction` is a recursive function that calculates the nth number in the Fibonacci sequence. The `memoizedExpensiveRecursiveFunction` is the memoized version of the `expensiveRecursiveFunction`.
